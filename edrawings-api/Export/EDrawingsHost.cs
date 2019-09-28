61c41c376b41782430cbbe0f24945ee6ecb8aaa5
CodeStack <codestack.net@gmail.com>
2019-09-28 14:18:55 +1000

    Added the eDrawings example to export to foreign format

---
+++ edrawings-api/Export/EDrawingsHost.cs
@@ -0,0 +1,36 @@
+﻿//**********************
+//Batch export files via eDrawings API (without SOLIDWORKS)
+//Copyright(C) 2019 www.codestack.net
+//License: https://github.com/codestack-net-dev/solidworks-api-examples/blob/master/LICENSE
+//Product URL: https://www.codestack.net/edrawings-api/output/export/
+//**********************
+
+using System;
+using System.Windows.Forms;
+using eDrawings.Interop.EModelViewControl;
+
+namespace Export
+{
+    public class EDrawingsHost : AxHost
+    {
+        public event Action<EModelViewControl> ControlLoaded;
+        private bool m_IsLoaded;
+
+        public EDrawingsHost() : base("22945A69-1191-4DCF-9E6F-409BDE94D101")
+        {
+            m_IsLoaded = false;
+        }
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+
+            if (!m_IsLoaded)
+            {
+                m_IsLoaded = true;
+                var ctrl = GetOcx() as EModelViewControl;
+                ControlLoaded?.Invoke(GetOcx() as EModelViewControl);
+            }
+        }
+    }
+}
---
+++ edrawings-api/Export/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Export")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CodeStack")]
+[assembly: AssemblyProduct("Export")]
+[assembly: AssemblyCopyright("Copyright © www.codestack.net 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c988df41-f3d6-4d78-ab2f-20785eacd664")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
